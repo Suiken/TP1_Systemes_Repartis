8e6a39a3adbf3e310d063d319306402f5502f258
Suiken <Kevin.Suy@me.com>
2016-05-17 06:51:48 +0200

    serverThread updated

--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -29,40 +29,18 @@ class ServerThread implements Runnable {
 
                 System.out.println("Le client numéro " + id + " est connecté !");
 
-                // Envoi message
-                PrintWriter out = new PrintWriter(socket.getOutputStream());
-                out.println("Votre ID est " + id);
-                out.flush();
+                sendMessage("Votre ID est " + id, socket);
 
                 // Reception message
-                InputStream socketInputStream = socket.getInputStream();
-
-                File file = new File("Calc.class");
-                ByteStream.toFile(socketInputStream, file);
-
-                BufferedReader in = new BufferedReader(new InputStreamReader(socketInputStream));
-                String message = in.readLine();
-
+                String message = receiveMessage(socket);
                 System.out.println("Message reçu de " + clientMap.get(socket) + " : " + message);
 
                 String[] messages = message.split("&");
+                Class<?> classOfFile = findClass(messages[0]);
 
-                File root = new File(".");
-                URLClassLoader classLoader = URLClassLoader.newInstance(new URL[] { root.toURI().toURL() });
-                Class<?> classOfFile = Class.forName(messages[0], true, classLoader);
-                Constructor<?> constructorOfFile = classOfFile.getConstructor();
-                Object object = constructorOfFile.newInstance(new Object[] { });
-                Method methods[] = classOfFile.getMethods();
-                Object result = new Object();
-                for(Method m : methods){
-                    if(m.getName().equals(messages[1])){
-                        result = m.invoke(object, messages[2].split(","));
-                    }
-                }
-
-
-                System.out.println("Fichier recu : " + file);
+                Object result = executeMethod(classOfFile, messages[1], messages[2].split(","));
                 System.out.println("Réponse à envoyer : " + result.toString());
+
                 ++id;
 
                 // Envoi message
@@ -76,6 +54,46 @@ class ServerThread implements Runnable {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    void sendMessage(String message, Socket socket) throws Exception{
+        PrintWriter out = new PrintWriter(socket.getOutputStream());
+        out.println("Votre ID est " + id);
+        out.flush();
+    }
+
+    String receiveMessage(Socket socket) throws Exception{
+        InputStream socketInputStream = socket.getInputStream();
+
+        File file = new File("Calc.class");
+        ByteStream.toFile(socketInputStream, file);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socketInputStream));
+        String message = in.readLine();
+
+        return message;
+
+    }
+
+    Class<?> findClass(String className) throws Exception{
+        File root = new File(".");
+        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[] { root.toURI().toURL() });
+        Class<?> classOfFile = Class.forName(className, true, classLoader);
+        return classOfFile;
+    }
+
+    Object executeMethod(Class<?> classOfFile, String methodName, String[] parameters) throws Exception{
+        Constructor<?> constructorOfFile = classOfFile.getConstructor();
+        Object object = constructorOfFile.newInstance(new Object[] { });
+
+        Method methods[] = classOfFile.getMethods();
+        Object result = new Object();
+        for(Method m : methods){
+            if(m.getName().equals(methodName)){
+                result = m.invoke(object, parameters);
+            }
+        }
+        return result;
     }
 }
 
